191a8303f48989d468508db179f17727d109f899
wonjun0120 <wonjundero@gmail.com>
2024-07-16 22:33:11 +0900

    fix: docker, docker-compose 연결
    #10

--- src/test/java/com/getourhome/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/getourhome/userservice/UserServiceApplicationTests.java
@@ -1,11 +1,14 @@
 package com.getourhome.userservice;
 
+import com.getourhome.userservice.util.JwtTokenProvider;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class UserServiceApplicationTests {
-
+    @MockBean
+    private JwtTokenProvider jwtTokenProvider;
     @Test
     void contextLoads() {
     }
